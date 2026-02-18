b4fbec477be17a962399760353f19cde878eae0f
Tracy Cano <tracycano3.14@gmail.com>
2026-02-17 23:55:34 -0800

    deleted unneccessary files

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,59 +1,51 @@
-import React, {useRef, useEffect} from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 
+function InputWithLabel({ children, setInputValue, inputValue, editMode }) {
+  const inputRef = useRef(null);
 
-function InputWithLabel ( {
-    children, 
-    setInputValue,
-    inputValue,
-    editMode
-} ) {
+  const whitebox = {
+    padding: "10px 2px",
+    margin: "15px",
+    textAlign: "center",
+    border: "2px solid purple",
+    borderRadius: "14px",
+    width: "40%",
+  };
 
-    const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-    const whitebox = {
-        padding: "10px 2px",
-        margin: "15px",
-        textAlign: "center",
-        border: "2px solid purple",
-        borderRadius: "14px",
-        width: "40%"
-    };
+  useEffect(() => {
+    if (editMode.status) {
+      setInputValue(editMode.data.fields.Title);
+    }
+  }, [editMode, setInputValue]);
 
-    useEffect(() => {
-            inputRef.current.focus();
-    }, []);
-
-    useEffect(() => {
-        if(editMode.status){
-            setInputValue(editMode.data.fields.Title);
-        }
-    }, [editMode, setInputValue])
-
-    return (
-        <>
-            <label htmlFor="title"> {children} </label>
-                <input 
-                    style = {whitebox}
-                    ref = {inputRef}
-                    type="text" 
-                    id="todoTitle"
-                    name="title"
-                    value={inputValue}
-                    onChange={ (event) => setInputValue(event.target.value) }
-                    />     
-         </>
-    );
-};
+  return (
+    <>
+      <label htmlFor="title"> {children} </label>
+      <input
+        style={whitebox}
+        ref={inputRef}
+        type="text"
+        id="todoTitle"
+        name="title"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+    </>
+  );
+}
 
 InputWithLabel.propTypes = {
-		children: PropTypes.string,
-        todoTitle: PropTypes.array,
-        handleTitleChange: PropTypes.func,
-        setInputValue: PropTypes.func,
-        inputValue: PropTypes.string,
-        editMode: PropTypes.object,
-
-}
+  children: PropTypes.string,
+  todoTitle: PropTypes.array,
+  handleTitleChange: PropTypes.func,
+  setInputValue: PropTypes.func,
+  inputValue: PropTypes.string,
+  editMode: PropTypes.object,
+};
 
 export default InputWithLabel;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,64 @@
 import React from "react";
-// import TodaysDate from './components/TodaysDate';
 
 function Navbar() {
-    
-    return (
-        <>
-        {/* <TodaysDate/> */}
-        <div 
+  return (
+    <>
+      <div
         style={{
-            fontSize: "20px",
-            padding: "20px",
-            height: "30px",
-            textAlign: "right",
-            backgroundColor: "#545454"
-            
-           }}
-           >  
-        <p style={{ position: "absolute",
-                    left: "auto",
-                    width: "50px",
-                    height: "60px"}}> </p>
+          fontSize: "20px",
+          padding: "20px",
+          height: "30px",
+          textAlign: "right",
+          backgroundColor: "#545454",
+        }}
+      >
+        <p
+          style={{
+            position: "absolute",
+            left: "auto",
+            width: "50px",
+            height: "60px",
+          }}
+        >
+          {" "}
+        </p>
         <ul style={{}}>
-                <li style={{
-                    listStyle: "none",
-                    display: "inline-block",
-                    color: "red"
-                    }} >
-                        
-                    <a style={{textDecoration: "none", 
-                                color: "#ff70d6", 
-                                letterSpacing: "2px" }} href='/mySchedule'> Home </a> 
+          <li
+            style={{
+              listStyle: "none",
+              display: "inline-block",
+              color: "red",
+            }}
+          >
+            <a
+              style={{
+                textDecoration: "none",
+                color: "#ff70d6",
+                letterSpacing: "2px",
+              }}
+              href="/mySchedule"
+            >
+              {" "}
+              Home{" "}
+            </a>
 
-                    <a style={{textDecoration: "none", 
-                                color: "#ff70d6", 
-                                letterSpacing: "2px", 
-                                wordSpacing: "2px"}} href='/'> &nbsp; TodoList &nbsp; </a>        
-                </li>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "#ff70d6",
+                letterSpacing: "2px",
+                wordSpacing: "2px",
+              }}
+              href="/"
+            >
+              {" "}
+              &nbsp; TodoList &nbsp;{" "}
+            </a>
+          </li>
         </ul>
-
-    </div>
+      </div>
     </>
-    )
+  );
 }
 
 export default Navbar;
